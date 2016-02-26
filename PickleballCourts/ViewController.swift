a12cd44a50cd52f6b1249ad5527f50a2542b0488
Ronny Hendrickx <ron.hendrickx@gmail.com>
2016-02-26 09:31:56 -0700

    various UI changes.. final version submitted to iTunes store

--- PickleballCourts/ViewController.swift
+++ PickleballCourts/ViewController.swift
@@ -7,12 +7,32 @@
 //
 
 import UIKit
+import GameplayKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var defaultStepper: UIStepper!
+    
+    @IBOutlet weak var label: UILabel!
+    
+    @IBAction func assignButton(sender: UIButton) {
+        setPlayers(Int(defaultStepper.value))
+        
+    }
+    
+    @IBAction func valueDidCahnge(sender: UIStepper) {
+        
+        label.text = "\(Int(sender.value))"
+        
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        configureStepper()
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +40,34 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func configureStepper(){
+        defaultStepper.value = 1
+        //defaultStepper.addTarget(self, action: "valueDidChange:", forControlEvents: .ValueChanged)
+        label.text = "\(Int(defaultStepper.value))"
+    }
+    
+    func setPlayers(counter: Int){
+        
+        let count = counter
+        var players = [Int] (1...count)
+        players = GKRandomSource.sharedRandom().arrayByShufflingObjectsInArray(players) as! [Int]
+        var playersCounter = players.count
+        if playersCounter < 16 { playersCounter = 16}
+        
+        
+        for i in 0..<playersCounter {
+            
+            if let theLabel = self.view.viewWithTag(i+1) as? UILabel {
+                if i < players.count {
+                    theLabel.text = (String)(players[i])
+                } else {
+                    theLabel.text = ""
+                }
+            }
+            
+            
+        }
+    }
 
 }
 
